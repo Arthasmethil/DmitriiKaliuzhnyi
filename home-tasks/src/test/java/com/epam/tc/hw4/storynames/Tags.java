33a5b97fd93b852ed7b406a9c8126d752a8d5fd3
Dmitrii Kaliuzhnyi <dmitrii_kaliuzhnyi@epam.com>
2022-02-26 18:15:39 +0300

    Scope of homework4: -Add listener -Tags for Features and Stories

---
+++ home-tasks/src/test/java/com/epam/tc/hw4/storynames/Tags.java
@@ -0,0 +1,11 @@
+package com.epam.tc.hw4.storynames;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Tags {
+    public static final String SOFT_ASSERT_TEST_FEATURE = "Soft assertion test";
+    public static final String ASSERTJ_TEST_FEATURE = "AssertJ test";
+    public static final String INTEGRATION_SOFT_STORY = "Soft asserts exercise 1";
+    public static final String INTEGRATION_HARD_STORY = "Hard asserts exercise 2";
+}
